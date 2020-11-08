d2e530547969a3629064b84fbfb25006f28bd5cb
mghaby <mark.ghaby71@hotmail.com>
2020-11-08 16:56:10 +1100

    temp fixed the listing form user error

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -69,6 +69,6 @@ class ListingsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def listing_params
-      params.require(:listing).permit(:title, :description, :price, :listing_id)
+      params.require(:listing).permit(:title, :description, :price )
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    has_and_belongs_to_many :listing
+  # has_many :listing
 end
