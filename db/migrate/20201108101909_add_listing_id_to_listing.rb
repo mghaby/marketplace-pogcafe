990c62caab53c8dd22d91082d3787f4ec9c54415
mghaby <mark.ghaby71@hotmail.com>
2020-11-09 11:22:32 +1100

    foreign key added, id in user to user_id in listings

--- db/migrate/20201108101909_add_listing_id_to_listing.rb
+++
@@ -1,5 +0,0 @@
-class AddListingIdToListing < ActiveRecord::Migration[6.0]
-  def change
-    add_foreign_key :listing_id, :listing
-  end
-end
---
+++ db/migrate/20201108234357_add_creator_id_to_user.rb
@@ -0,0 +1,5 @@
+class AddCreatorIdToUser < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :creator_id, :integer
+  end
+end
